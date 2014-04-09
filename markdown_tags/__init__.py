5c932149b7000464d2d5da4f16c2808da0e54863
Roman A. Taycher <rtaycher1987@gmail.com>
2014-04-09 07:23:41 -0700

    Tests are all passing

---
+++ markdown_tags/__init__.py
@@ -0,0 +1,10 @@
+from markdown_tags import *
+import os.path
+
+__author__ = "Roman A. Taycher"
+__copyright__ = "Copyright 2014, Roman A. Taycher"
+__credits__ = ["Roman A. Taycher"]
+__license__ = "MIT"
+__version__ = open(os.path.join(os.path.dirname(__file__), "VERSION")).read()
+__maintainer__ = "Roman A. Taycher"
+__email__ = "rtaycher1987@gmail.com"
--- markdown_tags/test/markdown_tags_test.py
+++ markdown_tags/test/markdown_tags_test.py
@@ -33,7 +33,7 @@ def compile_markdown(markdown_txt):
 
 class Test_MarkdownTagsComplicated(unittest.TestCase):
     def test_bold_italic_text(self):
-        tags = m.MD(m.Bold(m.Italic("This is important!!!")))
+        tags = m.MD(m.Paragraph(m.Bold(m.Italic("This is important!!!"))))
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
         #self.assertEqual("***This is important!!!***", markdown)
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
@@ -48,7 +48,7 @@ class Test_MarkdownTagsComplicated(unittest.TestCase):
         self.assertEqual(italic[0].text, "This is important!!!")
 
     def test_paragraphs(self):
-        tags = m.Md(m.Paragraph(m.Italic("Italian"), m.Bold("Boring, Portland")))
+        tags = m.MD(m.Paragraph(m.Italic("Italian")), m.Paragraph(m.Bold("Boring, Portland")))
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
         print("markdown_str is :\n" + markdown_str)
         print("compile_markdown(markdown_str) is :\n" + compile_markdown(markdown_str))
@@ -67,7 +67,7 @@ class Test_MarkdownTagsComplicated(unittest.TestCase):
 
 class Test_MarkdownTagsSingle(unittest.TestCase):
     def test_paragraphs(self):
-        tags = m.MD(m.Paragraph(s) for s in ["First","Security", "Tree"])
+        tags = m.MD(*[m.Paragraph(s) for s in ["First","Security", "Tree"]])
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
@@ -79,9 +79,7 @@ class Test_MarkdownTagsSingle(unittest.TestCase):
         self.assertEqual(paragraphs[2].text, "Tree")
 
     def test_hr(self):
-        tags = m.MD(m.Paragraph("First"),
-                m.HorizontalRuleLine,
-                m.Paragraph("Security"))
+        tags = m.MD(m.HorizontalRuleLine())
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
@@ -89,24 +87,24 @@ class Test_MarkdownTagsSingle(unittest.TestCase):
         self.assertIsNotNone(horizontal_rule, "there should be a horizontal line(rule)")
 
     def test_italic(self):
-        tags = m.MD(m.Italic("I"))
+        tags = m.MD(m.Paragraph(m.Italic("I")))
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
         emphasis = html("em")
 
         self.assertIsNotNone(emphasis, "there should be emphasis")
-        self.assertEqual(emphasis.text, "I")
+        self.assertEqual(emphasis.text(), "I")
 
     def test_bold(self):
-        tags = m.MD(m.Bold("B"))
+        tags = m.MD(m.Paragraph(m.Bold("B")))
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
         strong = html("strong")
 
         self.assertIsNotNone(strong, "there should be strong tags")
-        self.assertEqual(strong.text, "B")
+        self.assertEqual(strong.text(), "B")
 
     def test_unordered_list(self):
         tags = m.MD(m.UnorderedList("A", "B", "C"))
@@ -116,8 +114,7 @@ class Test_MarkdownTagsSingle(unittest.TestCase):
         unordered_lists = html("ul")
         self.assertEqual(1, len(unordered_lists))
 
-        unordered_list = unordered_lists[0]
-        items = unordered_list("li")
+        items = unordered_lists("li")
         self.assertEqual(3, len(items))
 
         self.assertEqual(["A","B","C"], [i.text for i in items])
@@ -130,52 +127,51 @@ class Test_MarkdownTagsSingle(unittest.TestCase):
         ordered_lists = html("ol")
         self.assertEqual(1, len(ordered_lists))
 
-        ordered_list = ordered_lists[0]
-        items = ordered_list("li")
+        items = ordered_lists("li")
         self.assertEqual(3, len(items))
 
         self.assertEqual(["A","B","C"], [i.text for i in items])
 
     def test_superscript(self):
-        tags = m.MD("5 squared ","5",m.Superscript("2"))
+        tags = m.MD(m.Paragraph("5 squared ","5",m.Superscript("2")))
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
         p = html("sup")
         self.assertIsNotNone(p)
-        self.assertEqual("2", p.text)
+        self.assertEqual("2", p.text())
 
     def test_link(self):
-        tags = m.MD(m.Link("example.com", "The example website"))
+        tags = m.MD(m.Paragraph(m.Link("example.com", "The example website")))
         markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
         link = html("a")
         link_url = link.attr("href")
-        link_text = link.text
+        link_text = link.text()
 
         self.assertEqual("example.com", link_url )
         self.assertEqual("The example website", link_text)
 
     def test_image(self):
-        tags = m.MD(m.Link("./pic1", "pic 1"))
-        markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
+        tags = m.MD(m.Paragraph(m.Image("./pic1", "pic 1")))
+        markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.basic)
 
         html = pq(compile_markdown(markdown_str), parser='html_fragments')
-        link = html("a")
-        link_url = link.attr("url")
-        link_alt = link.attr("alt")
+        img = html("img")
+        img_url = img.attr("src")
+        img_alt = img.attr("alt")
 
-        self.assertEqual("./pic1", link_url)
-        self.assertEqual("pic 1", link_alt)
+        self.assertEqual("./pic1", img_url)
+        self.assertEqual("pic 1", img_alt)
 
 
 
 class Test_RedditMarkdown(unittest.TestCase):
     def test_reddit_disallows_images(self):
         with self.assertRaises(m.IllegalMarkdownFormattingException):
-            tags = m.Paragraph(m.Image("http://en.wikipedia.org/wiki/File:Example.jpg", "Example Image"), "yo")
-            markdown_str = m.tags_to_markdown(tags, format_md="reddit")
+            tags = m.MD(m.Paragraph(m.Image("http://en.wikipedia.org/wiki/File:Example.jpg", "Example Image"), "yo"))
+            markdown_str = tags.tags_to_markdown(recover=False, format_md=m.MarkdownFormats.reddit)
             html = pq(compile_markdown(markdown_str), parser='html_fragments')
 
 
